905da0e376878aaa359913699373707e3c85642f
ALEXANDRA TOPIC <alexandratopic@college.harvard.edu>
2022-11-07 23:01:28 -0500

    ok

--- assignment2/match.py
+++ assignment2/match.py
@@ -1,7 +1,113 @@
 import numpy as np
 from typing import List, Tuple
+import random
+
+def changeArr(input1):
+ 
+    # Copy input array into newArray
+    newArray = input1.copy()
+     
+    # Sort newArray[] in ascending order
+    newArray.sort()
+     
+    # Dictionary to store the rank of
+    # the array element
+    ranks = {}
+     
+    rank = 1
+     
+    for index in range(len(newArray)):
+        element = newArray[index];
+     
+        # Update rank of element
+        if element not in ranks:
+            ranks[element] = rank
+            rank += 1
+         
+    # Assign ranks to elements
+    for index in range(len(input1)):
+        element = input1[index]
+        input1[index] = ranks[input1[index]]
 
 def run_matching(scores: List[List], gender_id: List, gender_pref: List) -> List[Tuple]:
+    # Edit Ranking
+    for i in range(len(scores)):
+        changeArr(scores[i])
+    # print(gender_pref)
+    print(scores)
+    
+    # Set Gender Preferences for Incompatible Ranking
+    for i in range(len(scores)):        
+        for j in range(len(scores[i])):
+            if scores[i][j] == 0:
+                continue
+            if gender_pref[i] == "Men" and genders[j] == "Female":
+                scores[i][j] = 0
+                scores[j][i] = 0
+                print(gender_pref[i], genders[j])
+            if gender_pref[i] =="Women" and genders[j] == "Male":
+                scores[i][j] = 0
+                scores[j][i] = 0
+            # if Bisexual, can be matched with anyone
+
+    proposed = [[], [], [], [], [], [], [], [], [], [], []]
+    # intialize everyone to be free
+    unmatched = []
+    for i in range(len(scores)):
+        unmatched.append(i)
+    print(unmatched)
+    matched = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
+    proposers = random.sample(range(0, 10), 5)
+    receivers = []
+    for i in range(len(scores)):
+        if i not in proposers:
+            receivers.append(i)
+    print("proposers", proposers, "receivers", receivers)
+  
+    
+    while any(item in proposers for item in unmatched):
+        proposee = proposers.pop()
+        receiver = 0
+        for j in range(len(scores[proposee])):
+            if scores[proposee][j] != 0 and j not in proposed[proposee]: 
+                receiver = j
+                break
+        
+        # if w if free, match m and w
+        if receiver not in matched:
+            matched[proposee] = receiver
+            matched[receiver] = proposee
+            unmatched.remove(receiver)
+            continue
+        # if w prefers m to her current match m' then free up m'
+        elif scores[receiver][matched[receiver]] > scores[receiver][proposee]:
+            matched[receiver] = proposee
+            matched[proposee] = receiver
+            # free up current match
+            matched[matched[receiver]] = 0
+            unmatched.append(matched[receiver])
+            unmatched.remove(proposee)
+        else:
+            # w rejects m
+            proposed[proposee].append(receiver)
+            proposers.append(proposee)
+    
+    print(matched)
+    for i in range(len(matched)):
+        if matched[i] == 10:
+            continue
+        else:
+            print(i, matched[i], genders[i], gender_pref[i], genders[matched[i]], gender_pref[matched[i]])
+    
+    print(gender[ ])
+
+    
+    
+                
+        
+
+
+
     """
     TODO: Implement Gale-Shapley stable matching!
     :param scores: raw N x N matrix of compatibility scores. Use this to derive a preference rankings.
@@ -21,6 +127,11 @@ def run_matching(scores: List[List], gender_id: List, gender_pref: List) -> List
             - What data structure can you use to take advantage of this fact when forming your matches?
         - This is by no means an exhaustive list, feel free to reach out to us for more help!
     """
+    # gay men should not be matched with straight men, set the scores of such combinations to be 0
+    # choose a random half of users to act as proposers
+    # proposers = random half
+    # other half of users
+    # receivers = 
     matches = [()]
     return matches
 
